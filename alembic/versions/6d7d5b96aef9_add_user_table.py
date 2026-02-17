5ec54f977aced342de164d0f5da5cc7529a5c076
Adlai M.B <adlaimb@gmail.com>
2026-02-16 20:06:03 -0800

    initial commit

---
+++ alembic/versions/6d7d5b96aef9_add_user_table.py
@@ -0,0 +1,37 @@
+"""add user table
+
+Revision ID: 6d7d5b96aef9
+Revises: 1cb784b6197f
+Create Date: 2026-02-16 18:33:03.474500
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6d7d5b96aef9'
+down_revision: Union[str, Sequence[str], None] = '1cb784b6197f'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table('users', 
+                    sa.Column('id', sa.Integer(), nullable=False), 
+                    sa.Column('email', sa.String(), nullable=False), 
+                    sa.Column('password', sa.String(), nullable=False), 
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True), 
+                              server_default=sa.text('now()'), nullable=False), 
+                    sa.PrimaryKeyConstraint('id'), 
+                    sa.UniqueConstraint('email'))
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table('users')
+    pass
